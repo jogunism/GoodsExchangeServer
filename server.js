10e0d09ef444288a2e7a049e1b111e7d0f01cd85
Ian Cho <jogunism@gmail.com>
2017-08-29 18:09:05 +0900

    Update server.js

--- server.js
+++ server.js
@@ -6,41 +6,40 @@
  * Copyright © raize.com All rights reserved.
  **/
 
-var express = require('express');
-var errorHandler = require('errorhandler');
-var bodyParser = require('body-parser');
-var http = require('http')
-var https = require('https');
-var fs = require('fs');
+// 3rd-party module
+var express 		= require('express');
+var bodyParser 		= require('body-parser');
+var fs 				= require('fs');
+var http 			= require('http');
+var https 			= require('https');
 
-var app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended : true }));
-app.use(errorHandler({ log : errorHandling }));
+// in-house module
+var prop 			= require('./properties');
+var constants 		= require('./constants');
+var AEApiClient 	= require('./api_client/appengine');
+var KTApiClient 	= require('./api_client/ktmhow');
+var logger 			= require('./manager/logger');
+var url 			= require('./manager/url');
 
-var CORSDomains = ['http://local.leadme.today:8080', 'http://192.168.0.28:8080', 'https://raizegls.appspot.com'];
+/**
+ *	환경변수 참조해서 시스템 런타임 프로퍼티 설정
+ *
+ */
+var isTest = process.argv.length > 2;
+var serverPhase = isTest? constants.SERVER_PHASE_DEV : process.env.KTM_SERVER_PHASE || constants.SERVER_PHASE_REAL;
 
-app.use(function(req, res, next) {
+prop.appengine.lmMode = serverPhase;
+prop.appengine.domain = process.env.LM_SERVER_DOMAIN;
+prop.ktmhow.isDevMode = (serverPhase == constants.SERVER_PHASE_DEV || serverPhase == constants.SERVER_PHASE_LOCAL)
 
-	var origin = req.header('Origin');
+// 환경변수 영향을 받는 URLManager 초기화
+url.init();
 
-	if(CORSDomains.indexOf(origin) >= 0){
-		res.header('Access-Control-Allow-Origin', origin);
-	}
-
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type,LM-mode');
-    next();
-});
-
-var prop = require('./properties');
-var AEApiClient = require('./api_client/appengine');
-var KTApiClient = require('./api_client/ktmhow');
-var logger = require('./manager/logger');
-
-
-///// utils ////////
+//
+// restfull api
+//
 var resSendToJson = function(res, obj) {
+
 	if(obj && obj != undefined) {
 		if(obj.hasOwnProperty('success') && !obj.success) {
 			res.statusCode = 500;
@@ -51,22 +50,44 @@ var resSendToJson = function(res, obj) {
 	res.end();
 }
 
-var errorHandling = function(err, str, req) {
-	var _obj = {
-		title : 'Error in '+ req.method + ' ' + req.url,
-		message : str
-	};
-	logger.error(_obj);
-// 	console.log(_obj);
-}
-//////////////
+/**
+ * Express 웹 어플리케이션 생성
+ * 미들웨어 설정
+ * 라우팅 핸들러 설정
+ *
+ */
 
-//
-// restfull api
-//
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : true }));
+app.use(function(req, res, next) {
+
+	var origin = req.header('Origin');
+
+	if(prop.cors.domains.indexOf(origin) >= 0){
+		res.header('Access-Control-Allow-Origin', origin);
+	}
+
+    res.header('Access-Control-Allow-Methods', prop.cors.method);
+    res.header('Access-Control-Allow-Headers', prop.cors.header);
+    next();
+});
+
+
 app.get('/products', function(req, res) {
 
-	KTApiClient.getProducts()
+	KTApiClient.getProducts('default')
+		.then(function(arr) {
+			resSendToJson(res, arr);
+		})
+		.catch(function(err) {
+			resSendToJson(res, err);
+		});
+});
+
+app.get('/products2', function(req, res) {
+
+	KTApiClient.getProducts('expanded')
 		.then(function(arr) {
 			resSendToJson(res, arr);
 		})
@@ -89,6 +110,7 @@ app.get('/available', function(req, res) {
 });
 
 app.post('/order', function(req, res) {
+	var _type = req.body.type || 'default';
 	var _token = req.body.tr_id_token;
 	var _userId = req.body.user_id;
 	var _goodsId = req.body.goods_id;
@@ -97,14 +119,14 @@ app.post('/order', function(req, res) {
 	var _trId = '';
 	var _ctrId = '';
 
-	KTApiClient.getProducts(_goodsId)
+	KTApiClient.getProducts(_type, _goodsId)
 		.then(function(product){
 			var _obj = {
 				token : _token,
 				userId : _userId, 
 				goodsId : product.products.goods_id,
 				goodsName : product.products.goods_nm,
-				goodsPrice : product.products.total_price
+				goodsPrice : product.products.total_price || product.products.real_price
 			}
 			return AEApiClient.verifyUser(_obj);
 		})
@@ -130,13 +152,14 @@ app.post('/order', function(req, res) {
 					code : err.statCode || err.code,
 					reason : err.reason
 				}
-				AEApiClient.sendResultFail();
+				AEApiClient.sendResultFail(_obj);
 			}
 
 			//인증실패면 끝.
 			resSendToJson(res, err);
 		})
 		.catch(function(err) {
+
 // 			logger.warn('결재성공 > 피드백 실패');
 
 			setTimeout(function() {
@@ -186,38 +209,36 @@ app.get('/cancel', function(req, res) {
 
 });
 
-//////////////
 
-var options = {
-    key  : fs.readFileSync('cert/leadme.key'),
-    cert : fs.readFileSync('cert/leadme.crt')
+//////////////
+var server;
+var port = isTest? 9391 : prop.port;
+
+if(serverPhase == constants.SERVER_PHASE_LOCAL){
+
+	server = http.createServer(app);
+}else{
+
+	try{
+		server = https.createServer({
+			    key  : fs.readFileSync('cert/leadme.key'),
+			    cert : fs.readFileSync('cert/leadme.crt')
+			}, app);
+	}catch(e){
+
+		// Maybe Local
+		server = http.createServer(app);
+	}
 }
 
-https.createServer(options, app).listen(prop.port, function() {
-
-	var userVar = process.env.USER_VAR || 'real';
-
-	prop.appengine.lmMode = userVar;
-
-	if(userVar && userVar != undefined && userVar == 'local') {
-		prop.appengine.domain = process.env.USER_DOMAIN;
-	}
-
-	if(	userVar && userVar != undefined && 
-		(userVar == 'dev' || userVar == 'local') ) {
-		prop.ktmhow.isDevMode = true;
-	}
-	else {
-		prop.ktmhow.isDevMode = false;
-	}
+server.listen(port, function() {
 
 	console.log('-------------------------------');
 	console.log(' Node start. ');
+	console.log(' - port : '+ port);
 	console.log(' - kt_mhow devmode : '+ prop.ktmhow.isDevMode);
 	console.log(' - LM-mode : '+ prop.appengine.lmMode);
-	if(prop.appengine.lmMode == 'local') {
-		console.log(' - local domain : '+ prop.appengine.domain);	
-	}
+	console.log(' - LM-domain : '+ url.AEVerifyUser());	
 	console.log('-------------------------------');
 
 // 	logger.info('server start');
